0b9f864d2bf4e7131f849db5be538b74b862d602
Drazen Drinic <drazendrinic@hotmail.com>
2023-10-11 06:25:51 -0500

    Install: tailwindcss and set up basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hi from v46-tier2-team-15</h1>;
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+      <h1 className="text-5xl">Hi from v46-tier2-team-15</h1>
+    </div>
+  );
 }
 
 export default App;
