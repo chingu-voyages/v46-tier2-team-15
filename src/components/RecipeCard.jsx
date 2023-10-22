760ea84663ffa05899a8f54d8b651a84024af4e8
Sadik Shaikh <abdullahsadik00@gmail.com>
2023-10-22 11:16:25 +0530

    Search Bar:[WIP]

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -2,3 +2,36 @@
 This component represents a 
 single recipe info in the list of recipes on the Search Page 
 */
+
+import React from "react";
+
+const RecipeCard = () => {
+  return (
+    <div className="p-10">
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <img className="w-full" src="/mountain.jpg" alt="Mountain" />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">Mountain</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, Nonea! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeCard;
