544afa85bbf6f81c1d8507a3e83e38a8fe456e96
Drazen Drinic <drazendrinic@hotmail.com>
2023-10-26 16:12:49 -0500

    fix: the path to images so they work when app is built

--- public/assets/contributors.js
+++ public/assets/contributors.js
@@ -2,7 +2,7 @@ const contributors = [
   //Darshan Pandya
   {
     name: "Darshan Pandya",
-    photo: "../../public/assets/contributors_photos/Darshan_Pandya.jpg",
+    photo: "./assets/contributors_photos/Darshan_Pandya.jpg",
     role: "Web Developer",
     aboutMe:
       "Hello there! I'm Darshan Pandya, hailing from the vibrant and diverse land of India. I'm an aspiring front-end developer. My passion lies in crafting captivating and responsive websites that come to life with the magic of HTML, CSS, JavaScript, and React.js. Apart from my coding adventures, I proudly wear the title of a football enthusiast, always up for a thrilling match or an engaging discussion about the beautiful game. And when I'm not immersed in the world of web development, you can often find me enjoying the captivating stories of Hollywood movies. So, whether it's lines of code or the lines delivered on the silver screen, I'm all about creating and appreciating remarkable experiences. ",
@@ -13,14 +13,14 @@ const contributors = [
         key: "GitHub",
         name: "Darshan-Pandya10",
         link: "https://github.com/Darshan-Pandya10",
-        icon: "../../public/assets/font_awesome_icons/github.svg",
+        icon: "./assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
         name: "Darshan Pandya",
         link: "https://www.linkedin.com/in/darshanpandya811",
-        icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
+        icon: "./assets/font_awesome_icons/linkedin-in.svg",
       },
     ],
   },
@@ -28,7 +28,7 @@ const contributors = [
   // Drazen Drinic
   {
     name: "Dražen Drinić",
-    photo: "../../public/assets/contributors_photos/Drazen_Drinic.jpeg",
+    photo: "./assets/contributors_photos/Drazen_Drinic.jpeg",
     role: "Web Developer",
     aboutMe:
       "Hello there! I'm Drazen Drinic, an accomplished Traffic Engineer with a proven track record of over a decade in the Transportation and Traffic industry. In addition to my expertise in Traffic Engineering, I have a deep-rooted passion for the world of IT. My diverse skill set encompasses various domains, including Unity Game Development, Java Web App Development, React Front-End Development, and sharing my knowledge through IT education. I'm driven by the ever-evolving nature of technology and constantly seek out opportunities to expand my skills and stay at the forefront of innovation. \n\nOutside of my professional pursuits, I find immeasurable joy in my role as a devoted husband and father. My two wonderful boys serve as a constant source of inspiration, fueling my desire to explore new frontiers within the intricate world of coding. Thank you for taking the time to visit my profile! If you have any questions or would like to connect, please feel free to reach out. \n\nSincerely, \nDrazen Drinic",
@@ -40,42 +40,42 @@ const contributors = [
         key: "GitHub",
         name: "Drazen-TheGRC",
         link: "https://github.com/Drazen-TheGRC",
-        icon: "../../public//assets/font_awesome_icons/github.svg",
+        icon: "./assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
         name: "Drazen Drinic",
         link: "https://www.linkedin.com/in/drazendrinic/",
-        icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
+        icon: "./assets/font_awesome_icons/linkedin-in.svg",
       },
       //Facebook
       {
         key: "Facebook",
         name: "Drazen Drinic",
         link: "https://www.facebook.com/drazen.drinic/",
-        icon: "../../public/assets/font_awesome_icons/facebook-f.svg",
+        icon: "./assets/font_awesome_icons/facebook-f.svg",
       },
       //Instagram
       {
         key: "Instagram",
         name: "drazen.drinic",
         link: "https://www.instagram.com/drazen.drinic/",
-        icon: "../../public/assets/font_awesome_icons/instagram.svg",
+        icon: "./assets/font_awesome_icons/instagram.svg",
       },
       //Twitter
       {
         key: "Twiter",
         name: "@Drazen_TheGRC",
         link: "https://twitter.com/Drazen_TheGRC",
-        icon: "../../public/assets/font_awesome_icons/twitter.svg",
+        icon: "./assets/font_awesome_icons/twitter.svg",
       },
     ],
   },
 
   {
     name: "Teodora Kočić",
-    photo: "s../../public/assets/contributors_photos/Teodora_Kocic.jpeg",
+    photo: "./assets/contributors_photos/Teodora_Kocic.jpeg",
     role: "Web Developer",
     aboutMe:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -86,21 +86,21 @@ const contributors = [
         key: "GitHub",
         name: "teodora-kocic",
         link: "https://github.com/teodora-kocic",
-        icon: "../../public/assets/font_awesome_icons/github.svg",
+        icon: "./assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
         name: "Teodora Kočić",
         link: "https://www.linkedin.com/in/teodora-kocic93/",
-        icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
+        icon: "./assets/font_awesome_icons/linkedin-in.svg",
       },
     ],
   },
 
   {
     name: "Circeag Petre",
-    photo: "../../public/assets/contributors_photos/Circeag_Petre.png",
+    photo: "./assets/contributors_photos/Circeag_Petre.png",
     role: "Product Owner",
     aboutMe:
       "As a Technical Product Owner with a background in front-end development, robotics, and two years of leadership volunteering in a robotics program, I bring a diverse skill set to drive successful product development. I'm well-versed in Scrum and Agile methodologies, ensuring efficient project management and delivery.",
@@ -111,14 +111,14 @@ const contributors = [
         key: "GitHub",
         name: "Petre4488",
         link: "https://github.com/Petre4488",
-        icon: "../../public/assets/font_awesome_icons/github.svg",
+        icon: "./assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
         name: "Petre Circeag",
         link: "https://www.linkedin.com/in/petre-circeag/",
-        icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
+        icon: "./assets/font_awesome_icons/linkedin-in.svg",
       },
     ],
   },
@@ -126,7 +126,7 @@ const contributors = [
   // Mohammad Waris
   {
     name: "Waris Fayiz",
-    photo: "../../public/assets/contributors_photos/Mohammad_Waris_Fayiz.jpg",
+    photo: "./assets/contributors_photos/Mohammad_Waris_Fayiz.jpg",
     role: "Web Developer",
     aboutMe:
       "Graduagted from school, getting bachlor's degree Computer Scince in Afghanistan, Balkh university. I love travling and want to learn more about cultures and religions.",
@@ -138,14 +138,14 @@ const contributors = [
         key: "GitHub",
         name: "fayiz770",
         link: "https://github.com/fayiz770",
-        icon: "../../public/assets/font_awesome_icons/github.svg",
+        icon: "./assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
         name: "Mohammad waris Fayiz",
         link: "https://www.linkedin.com/in/mohammad-waris-fayiz-8860b11a2/",
-        icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
+        icon: "./assets/font_awesome_icons/linkedin-in.svg",
       },
 
       //Instagram
@@ -153,14 +153,14 @@ const contributors = [
         key: "Instagram",
         name: "warisfayiz",
         link: "https://www.instagram.com/warisfayiz/",
-        icon: "../../public/assets/font_awesome_icons/instagram.svg",
+        icon: "./assets/font_awesome_icons/instagram.svg",
       },
     ],
   },
 
   {
     name: "Drazen Drinic",
-    photo: "../../public/assets/contributors_photos/Drazen_Drinic.jpeg",
+    photo: "./assets/contributors_photos/Drazen_Drinic.jpeg",
     role: "Web Developer",
     aboutMe:
       "Hello there! I'm Drazen Drinic, an accomplished Traffic Engineer with a proven track record of over a decade in the Transportation and Traffic industry. In addition to my expertise in Traffic Engineering, I have a deep-rooted passion for the world of IT. My diverse skill set encompasses various domains, including Unity Game Development, Java Web App Development, React Front-End Development, and sharing my knowledge through IT education. I'm driven by the ever-evolving nature of technology and constantly seek out opportunities to expand my skills and stay at the forefront of innovation. \n\nOutside of my professional pursuits, I find immeasurable joy in my role as a devoted husband and father. My two wonderful boys serve as a constant source of inspiration, fueling my desire to explore new frontiers within the intricate world of coding. Thank you for taking the time to visit my profile! If you have any questions or would like to connect, please feel free to reach out. \n\nSincerely, \nDrazen Drinic",
@@ -172,35 +172,35 @@ const contributors = [
         key: "GitHub",
         name: "Drazen-TheGRC",
         link: "https://github.com/Drazen-TheGRC",
-        icon: "../../public/assets/font_awesome_icons/github.svg",
+        icon: "./assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
         name: "Drazen Drinic",
         link: "https://www.linkedin.com/in/drazendrinic/",
-        icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
+        icon: "./assets/font_awesome_icons/linkedin-in.svg",
       },
       //Facebook
       {
         key: "Facebook",
         name: "Drazen Drinic",
         link: "https://www.facebook.com/drazen.drinic/",
-        icon: "../../public/assets/font_awesome_icons/facebook-f.svg",
+        icon: "./assets/font_awesome_icons/facebook-f.svg",
       },
       //Instagram
       {
         key: "Instagram",
         name: "drazen.drinic",
         link: "https://www.instagram.com/drazen.drinic/",
-        icon: "../../public/assets/font_awesome_icons/instagram.svg",
+        icon: "./assets/font_awesome_icons/instagram.svg",
       },
       //Twitter
       {
         key: "Twiter",
         name: "@Drazen_TheGRC",
         link: "https://twitter.com/Drazen_TheGRC",
-        icon: "../../public/assets/font_awesome_icons/twitter.svg",
+        icon: "./assets/font_awesome_icons/twitter.svg",
       },
     ],
   },
