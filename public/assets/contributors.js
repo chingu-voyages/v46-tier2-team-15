d4dfbbd95a17c6163a8a1d69d5b87e7bc2329f22
Drazen Drinic <drazendrinic@hotmail.com>
2023-10-26 06:30:36 -0500

    Add: app team member details and photo

--- public/assets/contributors.js
+++ public/assets/contributors.js
@@ -99,48 +99,26 @@ const contributors = [
   },
 
   {
-    name: "Drazen Drinic",
-    photo: "../../public/assets/contributors_photos/Drazen_Drinic.jpeg",
-    role: "Web Developer",
+    name: "Circeag Petre",
+    photo: "../../public/assets/contributors_photos/Circeag_Petre.png",
+    role: "Product Owner",
     aboutMe:
-      "Hello there! I'm Drazen Drinic, an accomplished Traffic Engineer with a proven track record of over a decade in the Transportation and Traffic industry. In addition to my expertise in Traffic Engineering, I have a deep-rooted passion for the world of IT. My diverse skill set encompasses various domains, including Unity Game Development, Java Web App Development, React Front-End Development, and sharing my knowledge through IT education. I'm driven by the ever-evolving nature of technology and constantly seek out opportunities to expand my skills and stay at the forefront of innovation. \n\nOutside of my professional pursuits, I find immeasurable joy in my role as a devoted husband and father. My two wonderful boys serve as a constant source of inspiration, fueling my desire to explore new frontiers within the intricate world of coding. Thank you for taking the time to visit my profile! If you have any questions or would like to connect, please feel free to reach out. \n\nSincerely, \nDrazen Drinic",
-
+      "As a Technical Product Owner with a background in front-end development, robotics, and two years of leadership volunteering in a robotics program, I bring a diverse skill set to drive successful product development. I'm well-versed in Scrum and Agile methodologies, ensuring efficient project management and delivery.",
     discordName: "Drazen-TheGRC",
     socialMedia: [
       //GitHub
       {
         key: "GitHub",
         name: "Petre4488",
-        link: "https://github.com/Drazen-TheGRC",
+        link: "https://github.com/Petre4488",
         icon: "../../public/assets/font_awesome_icons/github.svg",
       },
       //Linkedin
       {
         key: "LinkedIn",
-        name: "Drazen Drinic",
-        link: "https://www.linkedin.com/in/drazendrinic/",
+        name: "Petre Circeag",
+        link: "https://www.linkedin.com/in/petre-circeag/",
         icon: "../../public/assets/font_awesome_icons/linkedin-in.svg",
-      },
-      //Facebook
-      {
-        key: "Facebook",
-        name: "Drazen Drinic",
-        link: "https://www.facebook.com/drazen.drinic/",
-        icon: "../../public/assets/font_awesome_icons/facebook-f.svg",
-      },
-      //Instagram
-      {
-        key: "Instagram",
-        name: "drazen.drinic",
-        link: "https://www.instagram.com/drazen.drinic/",
-        icon: "../../public/assets/font_awesome_icons/instagram.svg",
-      },
-      //Twitter
-      {
-        key: "Twiter",
-        name: "@Drazen_TheGRC",
-        link: "https://twitter.com/Drazen_TheGRC",
-        icon: "../../public/assets/font_awesome_icons/twitter.svg",
       },
     ],
   },
