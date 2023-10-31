488e717ec2141eda3aee698e1c931e2f3c04109a
Mohammad Waris Fayiz <mohammadwaris456@gmail.com>
2023-10-31 22:25:01 +0330

    feature { about page }

---
+++ src/data/aboutPageContent.jsx
@@ -0,0 +1,50 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+export const contents = [
+    {
+        id: uuidv4(),
+        heading: 'Exploring the Culinary World',
+        text:   `Welcome to Dish Dive, your culinary 
+                sanctuary in the digital world! With 
+                a treasure trove of more than 10,000
+                recipes and a passionate team of 6,
+                we are here to delight your taste buds 
+                and inspire your inner chef.`,
+    },
+    {
+        id: uuidv4(),
+        heading: 'Our Mission',
+        text:   `At Dish Dive, we're on a mission to bring
+                the world of delicious dishes to your kitchen. 
+                Whether you're a seasoned cook or just starting 
+                your culinary journey, our diverse collection of 
+                recipes is designed to cater to every palate. 
+                We believe that cooking is not just about food;
+                it's about creating experiences and memories with 
+                every meal.`,
+    },
+    {
+        id: uuidv4(),
+        heading: 'Our Recipe Collection',
+        text:   `With over 10,000 recipes and counting, Dish Dive 
+                is your go-to resource for all things food. From 
+                mouthwatering appetizers to hearty main courses and 
+                delectable desserts, our extensive recipe library covers 
+                a wide range of cuisines and cooking techniques. Whether 
+                you're looking for quick weeknight dinners, gourmet feasts, 
+                or dietary-specific meals, we've got you covered.`,
+    },
+    {
+        id: uuidv4(),
+        heading: 'Join Us on this Culinary Journey',
+        text:   `We invite you to join us on this exciting culinary 
+        adventure. Explore our recipes, engage with our community, 
+        and be a part of the Dish Dive family. Whether you're here 
+        to discover new flavors or to master the art of cooking, 
+        we're here to support you every step of the way.
+        Thank you for choosing Dish Dive as your culinary companion. 
+        Together, we'll explore, create, and savor the incredible 
+        world of food.`,
+    },
+
+]
