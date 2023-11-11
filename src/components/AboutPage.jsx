ea895f05976f92a08f5037b1f8c2037a356ef782
Darshan Pandya <80563657+Darshan-Pandya10@users.noreply.github.com>
2023-11-11 12:14:30 +0530

    [Add] about bg images that changes with themes.

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,16 +1,21 @@
 import { contents } from "../data/aboutPageContent";
+import { ThemeContext } from '../App';
+import { useContext } from "react";
+
 /*
 This page will open between Header and Footer and 
 it will be a page about the Dish Dive App.
 */
 function AboutPage() {
+    const themeData = useContext(ThemeContext);
+    const {theme , toggleTheme} = themeData;
   const contentsArray = contents.map((contentItem) => {
     return (
-      <div key={contentItem.id}>
-        <h3 className="text-white text-left text-[18px] lg:text-[24px] xl:text-[32px] mb-[10px] font-bold">
+      <div key={contentItem.id}  className="about-text">
+        <h3 className=" text-left text-[18px] lg:text-[24px] xl:text-[32px] mb-[10px] font-bold">
           {contentItem.heading}
         </h3>
-        <p className="text-white text-left text-[14px] lg:text-[16px] xl:text-[20px] mb-[20px] md:w-2/4">
+        <p className=" text-left text-[14px] lg:text-[16px] xl:text-[20px] mb-[20px] md:w-2/4">
           {contentItem.text}
         </p>
       </div>
@@ -18,10 +23,11 @@ function AboutPage() {
   });
   return (
     <div className="w-full h-[100%] flex flex-col overflow-auto scrollbar-hide">
-      <div className="w-full bg-[#1B1107]">
-        <div className="px-[60px] md:px-[80px] py-[32px] xl:px-[120px] pb-[100px] bg-[url('/src/assets/page-images/hero-section.png')]  bg-cover opacity-90">
+      {/* <div className="w-full bg-[#1B1107] "> */}
+        <div className={`px-[60px] md:px-[80px] py-[32px] xl:px-[120px] pb-[100px] ${theme === 'dark' ? 'bg-[url(\'/src/assets/page-images/About-bg-dark.jpeg\')]' : 'bg-[url(\'/src/assets/page-images/About-bg-light.jpeg\')]' }  bg-cover`}>
+        
           {contentsArray}
-        </div>
+        {/* </div> */}
       </div>
     </div>
   );
